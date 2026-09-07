f84577aec0c4ae2669cb230f76f52be6e27ae11b
Florecimiento <florecimientointegraldelser@outlook.com>
2026-09-06 20:58:22 -0600

    Add files via upload

--- js/Registro.js
+++ js/Registro.js
@@ -26,7 +26,7 @@ document.getElementById("registerForm").addEventListener("submit", async (e) => 
   };
 
   try {
-    const response = await fetch("https://kefir.onrender.com/api/usuarios", {
+    const response = await fetch("https://kefir.onrender.com/api/registro", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
